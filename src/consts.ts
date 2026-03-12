b6e3838211e703aa9157b9df2a4faaca970691cc
archit dharod <archi.parchi7@gmail.com>
2026-03-12 20:04:55 +0100

    SEO related updates

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Jasmine Fan';
-export const SITE_DESCRIPTION = `Experience the innovative choreography and dance films of Taiwanese-born dancer, filmmaker and founder of TANZAHOi Internation Festival, Jasmine Fan.`;
+export const SITE_TITLE = 'Jasmine Fan | Choreographer, Curator & Filmmaker';
+export const SITE_DESCRIPTION = `Experience the innovative choreography and dance films of Taiwanese-born dancer, filmmaker and founder of TANZAHOi International Festival, Jasmine Fan.`;
